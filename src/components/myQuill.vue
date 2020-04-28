800812902463181e25e385d7c5e777cee671af1a
peizhongli <peizl1@lenovo.com>
2020-04-28 18:01:07 +0800

    因为要使用quill2.0所以不用vue-editor了 模仿封一个

--- src/components/myQuill.vue
+++ src/components/myQuill.vue
@@ -7,7 +7,9 @@
 </template>
 
 <script>
+// 引入Quill@2.0.0核心组件
 import Quill from 'quill'
+// 引入样式
 import 'quill/dist/quill.snow.css'
 import config from './quillModule/config2.js'
 import toolbar from './quillModule/toolbar.vue'
@@ -17,23 +19,27 @@ export default {
   components: {
     toolbar,
   },
+  // 双向绑定
   props: {
-    value: Object
+    value: String
   },
   data() {
     return {
-      quill:null,
+      // 初始化编辑器后绑定到quill参数上
+      quill: null,
       options: {
         theme: 'snow',
         modules: {
-            toolbar: config,
-            table: true,
+          toolbar: config,
+          table: true,
         },
         placeholder: '点击输入 ...'
     },
     }
   },
   mounted() {
+    // 初始化编辑器
+    this._initEditor()
     this.quill = new Quill(this.$refs.editor, this.options);
     this.quill.setContents(this.value)
     this.$el.querySelector('.ql-table-insert-row').innerHTML = `—`
@@ -43,6 +49,50 @@ export default {
     this.quill.on('text-change', () => {
       this.$emit('input', this.quill.getContents())
     });
-  }
+  },
+  methods: {
+    _initEditor() {
+      // 初始化编辑器 传入dom和option
+      this.quill = new Quill(this.$refs.editor, this.options);
+      // 双向绑定
+      this.quill.on('text-change', () => {
+        const quill = this.quill
+        let text = this.quill.getText()
+        let html = this.quill.root.innerHTML
+
+        this.$emit('input', {html,quill,text})
+      })
+      this.quill.on('selection-change', range => {
+        if (!range) {
+          this.$emit('blur', this.quill)
+        } else {
+          this.$emit('focus', this.quill)
+        }
+      })
+    }
+  },
+  watch: {
+    // 监听外部值的传入，用于将值赋予编辑器
+    value (newVal, oldVal) {
+      // 如果编辑器没有初始化，则停止赋值
+      if (!this.editor) {
+        return
+      }
+
+      // 获取编辑器当前内容
+      let content = this.editor.root.innerHTML
+
+      // 外部传入了新值，而且与当前编辑器的内容不一致
+      if (newVal && newVal !== content) {
+        // 将外部传入的HTML内容转换成编辑器识别的delta对象
+        let delta = this.editor.clipboard.convert({
+          html: newVal
+        })
+
+        // 编辑器的内容需要接收delta对象
+        this.editor.setContents(delta)
+      }
+    }
+  },
 }
 </script>
